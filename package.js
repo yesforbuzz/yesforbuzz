22f8e47340ef02ea98718fcb7cf431f7321cf53c
yesforbuzz <yesforbuzz@MacBook-Pro-2.local>
2023-08-07 03:37:03 +0800

    first commit

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "foundation-libsass-template",
+  "version": "0.0.1",
+  "devDependencies": {
+    "grunt": "latest",
+    "grunt-contrib-connect": "latest",
+    "grunt-contrib-watch": "latest",
+    "grunt-exec": "latest",
+    "grunt-gh-pages": "latest",
+    "grunt-sass": "latest",
+    "node-sass": "latest"
+  },
+  "dependencies": {
+    "bower": "^1.8.2",
+    "foundation": "^4.2.1-1",
+    "foundation-sites": "^6.7.4",
+    "saas": "^1.0.0"
+  }
+}
